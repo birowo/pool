47184468e577d95d7b7c9eacc5bff3709d760d05
birowo <1340zxcvbnm@gmail.com>
2025-09-02 22:01:43 +0700

    commit

--- pool.go
+++ pool.go
@@ -1,42 +1,43 @@
 package pool
 
 import (
-	"sync"
+	"sync/atomic"
 )
 
 type pool[T any] struct {
-	sync.Mutex
-	ts   []T
-	i, n uint
-	f    func() T
+	ts []T
+	atomic.Int32
+	int32
+	f func() T
 }
 
-func New[T any](n uint, f func() T) *pool[T] {
+func New[T any](n int32, f func() T) *pool[T] {
 	n = 1 << n
-	if n == 0 {
+	if n < 1 {
 		return nil
 	}
 	return &pool[T]{
-		sync.Mutex{},
 		make([]T, n),
-		0, n - 1,
+		atomic.Int32{},
+		n,
 		f,
 	}
 }
 func (p *pool[T]) Get() (r T) {
-	p.Lock()
-	if p.i == 0 {
+	i := p.Add(-1)
+	if i < 0 {
+		p.Store(0)
 		r = p.f()
 	} else {
-		p.i--
-		r = p.ts[p.i]
+		r = p.ts[i]
 	}
-	p.Unlock()
 	return
 }
 func (p *pool[T]) Put(r T) {
-	p.Lock()
-	p.ts[p.i] = r
-	p.i = (p.i + 1) & p.n
-	p.Unlock()
+	i := p.Add(1) - 1
+	if i > p.int32 {
+		p.Store(p.int32)
+		return
+	}
+	p.ts[i] = r
 }
