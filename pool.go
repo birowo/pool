878aaf95ea3193fab2fe96823be3b45f153fa66e
birowo <1340zxcvbnm@gmail.com>
2025-11-25 16:23:13 +0700

    commit

--- pool.go
+++ pool.go
@@ -4,31 +4,31 @@ import (
 	"sync/atomic"
 )
 
-type pool[T any] struct {
+type Pool[T any] struct {
 	ts []T
 	atomic.Int32
 	int32
 	fn func() T
 }
 
-func New[T any](n int32, fn func() T) *pool[T] {
+func New[T any](n int32, fn func() T) *Pool[T] {
 	if n == 0 {
 		return nil
 	}
-	return &pool[T]{
+	return &Pool[T]{
 		make([]T, n),
 		atomic.Int32{},
 		n,
 		fn,
 	}
 }
-func (p *pool[T]) Get() T {
+func (p *Pool[T]) Get() T {
 	if p.CompareAndSwap(0, 0) {
 		return p.fn()
 	}
 	return p.ts[p.Add(-1)]
 }
-func (p *pool[T]) Put(x T) {
+func (p *Pool[T]) Put(x T) {
 	if p.CompareAndSwap(p.int32, p.int32) {
 		return
 	}
